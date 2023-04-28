d16e0847b3808ecad952155fa8df2a3fb720d0aa
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-04-28 10:17:17 -0300

    atulização de links e dados da page

--- src/app/component/works/works.component.ts
+++ src/app/component/works/works.component.ts
@@ -11,12 +11,16 @@ export class WorksComponent {
   work2 ='';
   work3 ='';
   work4 ='';
+  work5 ='';
+  work6 ='';
 
   constructor(){
     this.work1 = 'assets/works/work1.png';
     this.work2 = 'assets/works/work2.png';
     this.work3 = 'assets/works/work3.png';
-    this.work4 = 'assets/works/work4.PNG'
+    this.work4 = 'assets/works/work4.PNG';
+    this.work5 = 'assets/works/work5.png';
+    this.work6 = 'assets/works/work6.PNG';
 
   }
 
