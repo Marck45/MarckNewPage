df58dc63fd46b23addc5f51c23993d4ddad487af
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-05-02 15:43:36 -0300

    script do menu hamburger

--- src/app/component/nav-aside-bar/nav-aside-bar.component.ts
+++ src/app/component/nav-aside-bar/nav-aside-bar.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckboxControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-aside-bar',
   templateUrl: './nav-aside-bar.component.html',
   styleUrls: ['./nav-aside-bar.component.css']
 })
-export class NavAsideBarComponent implements OnInit{
+export class NavAsideBarComponent implements OnInit {
 
-  img1:String = '';
+  img1: String = '';
   linkGithub = '';
   linkLinkedin = '';
   linkTwittwe = '';
+  
 
 
-  constructor(){
+  constructor() {
+
     this.img1 = 'assets/logo/logo.png';
     this.linkGithub = 'https://github.com/Marck45';
     this.linkLinkedin = 'https://www.linkedin.com/in/marcoscarvalho45/';
     this.linkTwittwe = 'https://twitter.com/marcosantcarva1';
+
   }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  closeMenu() {
+
+    let close = document.querySelector('#burger');
+
+    close?.classList.add('hide');
+
   }
 
 }
